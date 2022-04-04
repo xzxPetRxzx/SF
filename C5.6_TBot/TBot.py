1b84d13791e77d8f0c3c4d5d754655c97e9d7c9b
xzxPetRxzx <fat-bee@mail.ru>
2022-04-04 12:42:49 +1000

    Django project

--- C5.6_TBot/TBot.py
+++ C5.6_TBot/TBot.py
@@ -6,13 +6,15 @@ from config import TOKEN, keys
 
 bot = telebot.TeleBot(TOKEN)
 
+#Команды старт/хэлп
 @bot.message_handler(commands=['start', 'help'])
 def start(message: telebot.types.Message):
-    text = 'Для начала работы введите  команду в формате: \n<имя валюты>' \
-           '<в какую валюту перевести>' \
-           '<количество переводимой валюты>'
+    text = 'Для начала работы введите  команду в формате: \n<имя валюты, цену которой он хочет узнать>' \
+           '<имя валюты, в которой надо узнать цену первой валюты>' \
+           '<количество первой валюты>'
     bot.reply_to(message, text)
 
+#Команда вальюс
 @bot.message_handler(commands=['values'])
 def values(message: telebot.types.Message):
     text = 'Доступные валюты:'
@@ -20,14 +22,22 @@ def values(message: telebot.types.Message):
         text = '\n'.join((text,key,))
     bot.reply_to(message, text)
 
+#Обработчик текста
 @bot.message_handler(content_types = ['text',])
 def answer(message: telebot.types.Message):
-    values = message.text.split()
-    if len(values) > 3:
-        raise APIException('Слишком много параметров')
-    base, quote, amount = values
-    req = Converter.get_price(base, quote, amount)
-    text = req
-    bot.reply_to(message, text)
+    values = message.text.lower().split()
+#отлов ошибок
+    try:
+        if len(values) > 3:
+            raise APIException('Слишком много параметров')
+        base, quote, amount = values
+        req = Converter.get_price(base, quote, amount)
+    except APIException as e:
+        bot.reply_to(message, f'Ошибка пользователя:\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду:\n{e}')
+    else:
+        text = f'{amount} {base} в {quote} = {req}'
+        bot.reply_to(message, text)
 
 bot.polling()
